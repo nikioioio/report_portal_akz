b490c501fa5b2717d893d7abd81d8799780dc159
Nikita <myemail@example.com>
2021-07-14 19:58:02 +0600

    Первый коммит

--- margin_report/static/margin_report/js/main.js
+++ margin_report/static/margin_report/js/main.js
@@ -3,20 +3,71 @@ function get_val() {
     var data1 = new FormData()
     // data1.append('dcn', $('#docn').val())
     // data1.append('dc', $('#doc').val())
-    data1.append('test', control.files[0])
+    // console.log(control.files[1])
+    var arr = control.files
+    for (index = 0; index < arr.length; index++) {
+        if (arr[index].name == "АМП и АМД.xlsx") {
+            data1.append('test', arr[index])
+        }
+    }
+
+
+    // data1.append('test', control.files[0])
 
     var token = '{{ csrf_token }}';
-    alert('csrf generated');
+    // alert('csrf generated');
 
-    $.ajax({
-        type: 'POST',
-        url: 'upl/',
-        data: data1,
-        processData: false,
-        contentType: false,
-        headers: {'X-CSRFToken': token},
-        success: function (data) {
-            alert(data)
+    // $.ajax({
+    //     type: 'POST',
+    //     url: 'upl/',
+    //     data: data1,
+    //     processData: false,
+    //     contentType: false,
+    //     responseType: 'arraybuffer',
+    //     headers: {'X-CSRFToken': token},
+    //     success: function (data, status, xhr) {
+    //
+    //         var blob = new Blob([data], {type: 'application/vnd.ms-excel'});
+    //         console.log(blob)
+    //         var downloadUrl = URL.createObjectURL(blob);
+    //         var a = document.createElement("a");
+    //         a.href = downloadUrl;
+    //         a.download = 'download.xlsx';
+    //         document.body.appendChild(a);
+    //         a.click();
+    //         document.body.removeChild(a);
+    //
+    //     }
+    // })
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'upl/');
+    // xhr.setRequestHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function (e) {
+
+
+        if (this.status == 200) {
+            let disposition = this.getResponseHeader('Content-Disposition')
+
+            if (disposition && disposition.indexOf('attachment') !== -1) {
+                let filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                let matches = filenameRegex.exec(disposition);
+                if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+            }
+
+            var blob = new Blob([this.response], { type: 'application/vnd.ms-excel' });
+            var downloadUrl = URL.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.href = downloadUrl;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+        } else {
+            Materialize.toast('Invalid data!', 2000);
         }
-    })
+    }
+
+    xhr.send(data1);
 }
--- margin_report/views.py
+++ margin_report/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+import os
+from io import StringIO, BytesIO
+from typing import IO
+
+import xlsxwriter
+from django.http import HttpResponse, StreamingHttpResponse
 from django.shortcuts import render
 from django.utils.datastructures import MultiValueDictKeyError
 from django.views.decorators.csrf import csrf_exempt
@@ -16,9 +21,24 @@ def upload_files(request):
     if request.method == 'POST':
         try:
             file = request.FILES['test']
-            df = pd.read_excel(file)
-            print(df)
+            df = pd.read_excel(file,sheet_name='Для СС АМП',header=[0,1])
+            df = df.set_index('№ п/п')
+            # df = pd.DataFrame({'hhhh':[1,2,3]})
 
-            return HttpResponse(200)
+            output = BytesIO()
+
+            writer = pd.ExcelWriter(output, engine='xlsxwriter')
+            df.to_excel(writer, sheet_name='Sheet1')
+            writer.save()
+
+            output.seek(0)
+            # workbook = output.getvalue()
+
+            response = StreamingHttpResponse(output,
+                                             content_type='application/vnd.ms-excel')
+            response['Content-Disposition'] = f'attachment; filename=test.xlsx'
+
+            return response
+
         except MultiValueDictKeyError:
             return HttpResponse('При загрузке Файла произошла ошибка')
